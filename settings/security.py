f5c85af2309321a8af9608dfc6370ee8c841a529
Stefan hr Berder <stefan.berder@rechaoss.com>
2012-09-24 08:41:09 +0800

    fix comments for file paths

--- settings/security.py
+++ settings/security.py
@@ -1,4 +1,5 @@
 # global security settings
 # this file should host security variables like SECURITY_KEY or database
 # passwords.
-# For environment specific variables, create a settings.security_ENV module
+# For environment specific variables, create a
+# setting/security_RUN_ENV.py file
