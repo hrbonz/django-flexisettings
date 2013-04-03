3b8d59284b8b8d0b05dfbaa3a400b5927f4bee0c
Stefan hr Berder <stefan.berder@ledapei.com>
2013-04-03 11:44:03 +0800

    import wrapped settings
    * import settings through the proxy
    * create a dictionary to cache import results
    * add attribute access through __getattribute__()
    * rename proxy class to FlexiSettingsProxy

--- flexisettings/settings.py
+++ flexisettings/settings.py
@@ -2,12 +2,20 @@
 import os
 import sys
 
+from django.utils import importlib
+
 ENVIRONMENT_VARIABLE = "FLEXI_WRAPPED_MODULE"
 
 
-class SettingsProxy(object):
+class FlexiSettingsProxy(object):
+
+    _globals = {
+        # no running environment by default
+        'FLEXI_RUN_ENV': None,
+    }
+
     def __init__(self):
-        try: 
+        try:
             settings_module = os.environ[ENVIRONMENT_VARIABLE]
             if not settings_module:
                 raise KeyError
@@ -16,10 +24,33 @@ class SettingsProxy(object):
                 "because environment variable %s is undefined." \
                 % ENVIRONMENT_VARIABLE
             )
+        self._settings_module = settings_module
+
+        try:
+            mod = importlib.import_module(self._settings_module)
+        except ImportError, e:
+            raise ImportError("Could not import settings '%s': %s" \
+                % (self._settings_module, e)
+            )
+        self._mod = mod
+
+    # old-style class attribute lookup
+    def __getattr__(self, name):
+        return self.__getattribute__(name)
+
+    # new-style class attribute lookup
+    def __getattribute__(self, name):
+        # break recursion when calling private attributes
+        if name.startswith('_'):
+            return object.__getattribute__(self, name)
+        if name in self._globals:
+            return self._globals[name]
+
+        return object.__getattribute__(self, name)
 
 
 # trick to replace the module by a class instance
 # see http://mail.python.org/pipermail/python-ideas/2012-May/014969.html
 # after importing flexisettings.settings, the module will actually be an
 # instance of SettingsProxy.
-sys.modules[__name__] = SettingsProxy()
+sys.modules[__name__] = FlexiSettingsProxy()
