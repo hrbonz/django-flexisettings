7a94badfecd929028fb61d365c799dbc01c2833c
Stefan hr Berder <stefan.berder@ledapei.com>
2013-04-03 17:07:58 +0800

    add setUpClass and tearDownClass in tests.base

--- tests/base.py
+++ tests/base.py
@@ -12,19 +12,24 @@ class BaseTestCase(unittest2.TestCase):
     test_project = 'testProject'
     envvar = 'FLEXI_WRAPPED_MODULE'
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         # create test folder
-        os.mkdir(self.test_folder)
+        os.mkdir(cls.test_folder)
         # create a sample project
-        call_command('startproject', self.test_project, self.test_folder)
+        call_command('startproject', cls.test_project, cls.test_folder)
+        # add this location to sys.path for import
+        sys.path.insert(0, os.path.join(os.getcwd(), cls.test_folder))
+
+    def setUp(self):
         # change current directory to test folder
         os.chdir(self.test_folder)
-        # add this location to sys.path for import
-        sys.path.insert(0, os.getcwd())
 
     def tearDown(self):
         if self.envvar in os.environ:
             os.environ.pop(self.envvar)
-        sys.path.pop(0)
         os.chdir('..')
-        shutil.rmtree(self.test_folder)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.test_folder)
