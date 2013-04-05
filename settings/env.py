c5c64b160496c0511e87adf17a31fcb6858e26db
Stefan hr Berder <stefan.berder@ledapei.com>
2013-04-05 14:59:07 +0800

    get rid of the old code and mechanisms

--- settings/env.py
+++
@@ -1,2 +0,0 @@
-# environment declaration
-RUN_ENV = ''
--- settings/security.py
+++
@@ -1,5 +0,0 @@
-# global security settings
-# this file should host security variables like SECURITY_KEY or database
-# passwords.
-# For environment specific variables, create a
-# setting/security_RUN_ENV.py file
