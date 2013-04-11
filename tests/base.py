a8822f8f03d85454f2cadd9d6e69995241d31ccb
Stefan hr Berder <stefan.berder@ledapei.com>
2013-04-11 16:47:04 +0800

    add ugly hack for test suites
    fake project module unloading for test suites to work (the module would
    stay loaded in memory and 'startproject' in setUpClass would fail.

--- tests/base.py
+++ tests/base.py
@@ -18,36 +18,37 @@ class BaseTestCase(unittest.TestCase):
     def setUpClass(cls):
         # get current folder
         cls.current_dir = os.getcwd()
-        # create test folder
-        os.mkdir(cls.test_folder)
-        # create a sample project
-        call_command('startproject', cls.test_project, cls.test_folder)
-        # create environment configuration
-        env_fd = open(
-            os.path.join(cls.test_folder, cls.test_project, 'env.py'),
-            'w')
-        env_fd.write("FLEXI_RUN_ENV = 't'\n")
-        env_fd.close()
-        # create security file
-        sec_fd = open(
-            os.path.join(cls.test_folder, cls.test_project, 'security.py'),
-            'w')
-        sec_fd.write("_SECRET_KEY = '%s'\n" % cls.secret_key)
-        sec_fd.close()
-        # create local settings file
-        locset_fd = open(
-            os.path.join(cls.test_folder, cls.test_project, 'settings_t.py'),
-            'w')
-        locset_fd.write("SECRET_KEY = _SECRET_KEY\n")
-        locset_fd.write("FLEXI_LAYOUT_DISCOVERY = True\n")
-        locset_fd.close()
-        # add local apps, media, static, templates folders
-        os.mkdir(os.path.join(cls.test_folder, 'apps'))
-        os.mkdir(os.path.join(cls.test_folder, 'media'))
-        os.mkdir(os.path.join(cls.test_folder, 'static'))
-        os.mkdir(os.path.join(cls.test_folder, 'templates'))
-        # add this location to sys.path for import
-        sys.path.insert(0, os.path.join(os.getcwd(), cls.test_folder))
+        if not os.path.isdir(cls.test_folder):
+            # create test folder
+            os.mkdir(cls.test_folder)
+            # create a sample project
+            call_command('startproject', cls.test_project, cls.test_folder)
+            # create environment configuration
+            env_fd = open(
+                os.path.join(cls.test_folder, cls.test_project, 'env.py'),
+                'w')
+            env_fd.write("FLEXI_RUN_ENV = 't'\n")
+            env_fd.close()
+            # create security file
+            sec_fd = open(
+                os.path.join(cls.test_folder, cls.test_project, 'security.py'),
+                'w')
+            sec_fd.write("_SECRET_KEY = '%s'\n" % cls.secret_key)
+            sec_fd.close()
+            # create local settings file
+            locset_fd = open(
+                os.path.join(cls.test_folder, cls.test_project, 'settings_t.py'),
+                'w')
+            locset_fd.write("SECRET_KEY = _SECRET_KEY\n")
+            locset_fd.write("FLEXI_LAYOUT_DISCOVERY = True\n")
+            locset_fd.close()
+            # add local apps, media, static, templates folders
+            os.mkdir(os.path.join(cls.test_folder, 'apps'))
+            os.mkdir(os.path.join(cls.test_folder, 'media'))
+            os.mkdir(os.path.join(cls.test_folder, 'static'))
+            os.mkdir(os.path.join(cls.test_folder, 'templates'))
+            # add this location to sys.path for import
+            sys.path.insert(0, os.path.join(os.getcwd(), cls.test_folder))
 
     def setUp(self):
         # change current directory to test folder
@@ -60,4 +61,11 @@ class BaseTestCase(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        os.chdir(cls.current_dir)
         shutil.rmtree(cls.test_folder)
+        # THIS IS AN UGLY HACK
+        # if test_project is not removed, the next 'startproject'
+        # command will fail. It is not possible at the present time to
+        # really unload python modules.
+        if cls.test_project in sys.modules:
+            sys.modules.pop(cls.test_project)
